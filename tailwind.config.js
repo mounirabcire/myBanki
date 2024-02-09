5dea840035f5410120c82518006c7e7d8a0e88e7
mounirabcire <mounirwebdevcode@gmail.com>
2024-02-09 21:26:55 +0100

    updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 /* eslint-disable import/no-anonymous-default-export */
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+    // content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+    content: [
+        './pages/**/*.{js,ts,jsx,tsx}',
+        './components/**/*.{js,ts,jsx,tsx}',
+    ],
     theme: {
         colors: {
             transparent: 'transparent',
